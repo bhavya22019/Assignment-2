0a1e9257f5943c22224b69df1b613d36a442263b
M Bhavya Sri <79437545+bhavya22019@users.noreply.github.com>
2021-07-08 20:53:45 +0530

    Add files via upload

---
+++ assignmet2.js
@@ -0,0 +1,66 @@
+                                              ////////PROBLEM 1
+
+for(i=1;i<=100; i=i+1)
+	{
+  
+		if(((i%3) == 0) && ((i%5) != 0)) {
+					
+    console.log('Fizz')
+  
+		}
+
+  		else if (((i%3) != 0) && ((i%5) == 0)){
+
+    						console.log('Buzz')
+  
+		}
+		
+else if (((i%3) == 0) && ((i%5) == 0)){
+
+						 console.log('FizzBuzz')
+		
+}
+
+		else{
+
+			console.log(i)
+  
+		}
+
+}
+
+                
+
+                          			//////////PROBLEM-2
+
+s="aba abab aba abab"
+
+w=''
+
+r=''
+
+for (i of s){
+
+	  if (i==' ') {
+		
+    if (r==w) {
+			
+      console.log(w)
+		
+    }
+	
+    		w=''
+			
+r=''
+
+ 	 }
+
+	  else{
+
+		    w=w+i 
+
+		    r=i+r
+  
+	}
+
+}
